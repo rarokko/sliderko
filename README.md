ebb60b29328908dada2c872a6bbf6ef5a6428fdb
Gabriel <rarokko@gmail.com>
2020-01-28 18:20:57 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # sliderko
-A simple React horizontal slider with swipe gesture on both PC and mobile devices.
+
+> 
+
+[![NPM](https://img.shields.io/npm/v/sliderko.svg)](https://www.npmjs.com/package/sliderko) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save sliderko
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'sliderko'
+
+class Example extends Component {
+  render () {
+    return (
+      <MyComponent />
+    )
+  }
+}
+```
+
+## License
+
+MIT © [rarokko](https://github.com/rarokko)
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import styles from './styles.css'
+
+export default class Sliderko extends Component {
+
+  constructor() {
+      super();
+
+      this.div = React.createRef();
+      this.mouseInitialX = 0;
+      this.moving = false;
+  }
+
+  toggleBetweenMobileAndDesk() {
+      this.mobileCheck = navigator.userAgent.toLowerCase().indexOf("mobile") >= 0 ? true : false;
+
+      if (this.mobileCheck && this.div.current) {
+          this.div.current.classList.add(styles["sliderko-component-mobile"]);
+      } else if (this.div.current) {
+          this.div.current.classList.remove(styles["sliderko-component-mobile"]);
+      };
+  }
+
+  componentDidMount() {
+      window.addEventListener("resize", () => this.toggleBetweenMobileAndDesk(), false);
+      this.toggleBetweenMobileAndDesk.call(this);
+  }
+
+  onMouseDown(evt) {
+      this.mouseInitialX = evt.clientX;
+      if (!this.mobileCheck) this.moving = true;
+  }
+
+  onMouseMove(evt) {
+      if (this.moving == false) return;
+
+      let currentXPosition = evt.clientX;
+      let moveCalc = this.mouseInitialX - currentXPosition;
+      this.div.current.scrollLeft = this.div.current.scrollLeft + moveCalc;
+      this.mouseInitialX = currentXPosition;
+  }
+
+  onMouseUp(evt) {
+      this.moving = false;
+  }
+
+  render() {
+      return (
+          <div
+              ref={this.div}
+              onMouseDown={(evt) => this.onMouseDown(evt)}
+              onMouseMove={(evt) => this.onMouseMove(evt)}
+              onMouseUp={(evt) => this.onMouseUp(evt)}
+              onMouseLeave={(evt) => this.onMouseUp(evt)}
+              className={styles[`sliderko-component`]}
+          >
+              {this.props.children}
+          </div>
+      );
+  }
+}
